2952927d8184b25f408669cf8f1482a347e0a537
mahfuz110244 <mahfuzku11@gmail.com>
2022-04-02 00:37:10 +0600

    update channel code for singla stopped

--- channel.go
+++ channel.go
@@ -75,6 +75,42 @@
 // 	ch <- "stopped me if you can"
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func goRoutine(ch chan string) {
+// 	for {
+// 		fmt.Println("working, now wait for 1 second in goroutine")
+// 		time.Sleep(1 * time.Second)
+// 		fmt.Println(time.Now())
+// 		data := <-ch
+// 		fmt.Println(fmt.Sprintf("signal from main goroutine: %s", data))
+// 		if data != "stop" {
+// 			fmt.Println("working in goroutine")
+// 		} else {
+// 			fmt.Println("stopped in goroutine")
+// 			return
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	fmt.Println(fmt.Sprintf("main goroutine starting: %s", time.Now()))
+// 	ch := make(chan string)
+// 	go goRoutine(ch)
+// 	fmt.Println("wait for 20 second in main goroutine")
+// 	time.Sleep(20 * time.Second)
+// 	ch <- "stop"
+// 	time.Sleep(1 * time.Second)
+// 	fmt.Println(fmt.Sprintf("main goroutine finished: %s", time.Now()))
+// }
+
+// Create a goroutine that will loop and sleep for 1 second. When it wakes up it will print out the time.
+// The main routine will wait for 20 seconds and then send a termination signal to the goroutine.
 package main
 
 import (
@@ -82,18 +118,16 @@ import (
 	"time"
 )
 
-func goRoutine(ch chan string) {
+func goRoutine(quit chan string) {
 	for {
-		fmt.Println("working, now wait for 1 second in goroutine")
-		time.Sleep(1 * time.Second)
-		fmt.Println(time.Now())
-		data := <-ch
-		fmt.Println(fmt.Sprintf("signal from main goroutine: %s", data))
-		if data != "stop" {
-			fmt.Println("working in goroutine")
-		} else {
+		select {
+		case <-quit:
 			fmt.Println("stopped in goroutine")
 			return
+		default:
+			fmt.Println("working, now wait for 1 second in goroutine")
+			time.Sleep(1 * time.Second)
+			fmt.Println(time.Now())
 		}
 	}
 }
@@ -105,6 +139,5 @@ func main() {
 	fmt.Println("wait for 20 second in main goroutine")
 	time.Sleep(20 * time.Second)
 	ch <- "stop"
-	time.Sleep(1 * time.Second)
 	fmt.Println(fmt.Sprintf("main goroutine finished: %s", time.Now()))
 }
