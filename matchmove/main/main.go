b5ae1d8de64b75138b6063a22477bf5e9a213247
Mahfuzur Rahman Khan <mahfuzku11@gmail.com>
2022-11-01 22:46:50 +0600

    add golang htlmt template

--- matchmove/main/main.go
+++ matchmove/main/main.go
@@ -3,12 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	array := []int{}
-	reverseArray := []int{}
-	for i := len(array) - 1; i >= 0; i-- {
-		fmt.Println(array[i])
-		reverseArray = append(reverseArray, array[i])
-
-	}
-	fmt.Println(reverseArray)
+	
 }
